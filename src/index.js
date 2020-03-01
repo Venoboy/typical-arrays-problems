7d4f27652e019951141f7366e7826bcc8a7bbe86
Admin <d>
2020-03-01 20:51:54 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+
+    if (!array || array.length === 0) return 0;
+    let min = array[0];
+    array.forEach(elem => {
+       elem < min ? min = elem : '';
+    });
+  return min;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (!array || array.length === 0) return 0;
+    let max = array[0];
+    array.forEach(elem => {
+       elem > max ? max = elem : '';
+    });
+  return max;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!array || array.length === 0) return 0;
+    let sum = 0;
+    array.forEach(elem => {
+       sum += elem;
+    });
+  return sum / array.length;
+};
+
